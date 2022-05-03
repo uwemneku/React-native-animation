16b8ecce54d1454bc2b34c5c7ea1397e0de05174
Uwem Israel <iamuwem@gmail.com>
2022-05-03 19:53:19 +0100

    implemented animation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,128 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Dimensions } from "react-native";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from "react-native-gesture-handler";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
-export default function App() {
+const WIDTH = Dimensions.get("screen").width;
+const CONTAINER_WIDTH = WIDTH * 0.9;
+const NUMBER_OF_ICONS = 10;
+const PADDING = 10;
+const ICON_SIZE = CONTAINER_WIDTH / (NUMBER_OF_ICONS + 1);
+
+const getCurrentPosition = (value: number) => {
+  "worklet";
+  return Math.floor((value - PADDING * 2) / ICON_SIZE);
+};
+
+const App = () => {
+  const currentPosition = useSharedValue(-1); // -1 means no icon is selected
+  const resetPosition = () => {
+    "worklet";
+    currentPosition.value = -1;
+  };
+  const animatedGestureHandler = useAnimatedGestureHandler({
+    onActive({ x }) {
+      currentPosition.value = getCurrentPosition(x);
+    },
+    onEnd() {
+      resetPosition();
+    },
+    onCancel() {
+      resetPosition();
+    },
+    onFail() {
+      resetPosition();
+    },
+    onStart({ x }) {
+      currentPosition.value = getCurrentPosition(x);
+    },
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <PanGestureHandler onGestureEvent={animatedGestureHandler}>
+        <Animated.View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            backgroundColor: "white",
+            padding: PADDING,
+            borderRadius: PADDING,
+            width: CONTAINER_WIDTH,
+          }}
+        >
+          {new Array(NUMBER_OF_ICONS).fill(0).map((_, index) => (
+            <Box key={index} index={index} activeIndex={currentPosition} />
+          ))}
+        </Animated.View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
+};
+
+interface Props {
+  index: number;
+  activeIndex: Animated.SharedValue<number>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Box = ({ activeIndex, index }: Props) => {
+  const inputRange = [-1, 0, 7]; // input range is zero when activeIndex is equal to index;
+  const backgroundColor = index % 2 ? "red" : "purple";
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const isGestureInactive = activeIndex.value === -1;
+    const diff = isGestureInactive ? -1 : Math.abs(activeIndex.value - index);
+    return {
+      transform: [
+        {
+          scale: withSpring(
+            interpolate(diff, inputRange, [1, 2, 0.3], Extrapolate.CLAMP)
+          ),
+        },
+        {
+          translateY: withSpring(
+            interpolate(diff, inputRange, [0, -40, 0], Extrapolate.CLAMP)
+          ),
+        },
+      ],
+      zIndex: interpolate(diff, inputRange, [0, 10, 1], Extrapolate.CLAMP),
+      marginHorizontal: withSpring(
+        interpolate(diff, inputRange, [0, ICON_SIZE, 0], Extrapolate.CLAMP)
+      ),
+      elevation: withSpring(
+        interpolate(diff, inputRange, [0, 10, 0], Extrapolate.CLAMP)
+      ),
+    };
+  });
+  return (
+    <Animated.View
+      style={[
+        {
+          width: ICON_SIZE,
+          height: ICON_SIZE,
+          borderRadius: PADDING,
+          backgroundColor: backgroundColor,
+        },
+        animatedStyle,
+      ]}
+    />
+  );
+};
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,3 @@
+## SwiftUI Animation
+This is an attempt to recreate an animation initial created with Swift UI that was shared in twitter.
+
